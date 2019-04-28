3ad03877eca58c790b73c4e99d69e92b3544a658
xoreo <mattnappo@gmail.com>
2019-04-28 16:14:09 -0400

    database: Finished LoadDB.

--- database/serverdb.go
+++ database/serverdb.go
@@ -1,6 +1,9 @@
 package database
 
 import (
+	"encoding/json"
+	"io/ioutil"
+
 	"github.com/xoreo/mcserver-backend/types"
 )
 
@@ -10,4 +13,22 @@ const ServerDBName = "serverdb"
 // ServerDB contains the metadata for all of the servers.
 type ServerDB struct {
 	Servers []types.Server `json:"servers"`
+}
+
+// LoadDB returns the contents of a ServerDB file.
+func LoadDB() (*ServerDB, error) {
+	// Read the database file
+	rawRead, err := ioutil.ReadFile(ServerDBName + ".json")
+	if err != nil {
+		return nil, err
+	}
+
+	// Reconstruct the file
+	buffer := &ServerDB{}
+	err = json.Unmarshal(rawRead, buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	return buffer, nil // Return
 }
