8083c176761d6362e0518502f8649078e0df67a8
xoreo <mattnappo@gmail.com>
2019-05-27 21:18:12 -0400

    api: Wrote API functions outline.

--- api/api.go
+++ api/api.go
@@ -1,1 +1,40 @@
 package api
+
+// CreateServer is the api function to create a new server.
+func CreateServer() {
+
+}
+
+// SendCommand is the api function that sends a command to the server.
+func SendCommand() {
+
+}
+
+// EditProperties is the api function that edits the server.properties file.
+func EditProperties() {
+
+}
+
+/* ----- START SYSTEMCTL COMMANDS ----- */
+
+// StartServer is the api function that starts a server.
+func StartServer() {
+
+}
+
+// StopServer is the api function that stops a server.
+func StopServer() {
+
+}
+
+// RestartServer is the api function that restarts a server.
+func RestartServer() {
+
+}
+
+// ServerStatus is the api function that gets the status of a server.
+func ServerStatus() {
+
+}
+
+/* ----- END SYSTEMCTL COMMANDS ----- */
