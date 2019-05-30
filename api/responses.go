4170119b6fcefc8d4019dc7d7cfc4e8c28001c38
xoreo <mattnappo@gmail.com>
2019-05-29 23:24:11 -0400

    all: Cleanup.

--- api/responses.go
+++ api/responses.go
@@ -5,10 +5,23 @@ type GETResponse struct {
 	Output string `json:"output"`
 }
 
+// ErrorResponse is the structure for when an error is thrown on the server side.
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
 // NewGETResponse constructs a new GETResponse struct.
 func NewGETResponse(output string) GETResponse {
 	// Return the new response
 	return GETResponse{
 		Output: output,
+	}
+}
+
+// NewErrorResponse constructs an ErrorResponse.
+func NewErrorResponse(err string) ErrorResponse {
+	// Return the new response
+	return ErrorResponse{
+		Error: err,
 	}
 }
