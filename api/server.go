539a19e372096c1025aeffbbeeacbb7c42743caa
xoreo <mattnappo@gmail.com>
2020-01-03 15:56:28 -0500

    all: Wrote a readme.

--- api/server.go
+++ api/server.go
@@ -9,11 +9,11 @@ import (
 func StartAPIServer(port int) error {
 	api := NewAPI(port) // Create a new API
 
+	api.Log.Infof("API server to listen on port %d", port)
 	err := http.ListenAndServe(":"+strconv.Itoa(api.Port), api.Router) // Start an HTTP server
 	if err != nil {
 		return err
 	}
 
-	api.Log.Infof("API server listening on port %d", port)
 	return nil
 }
