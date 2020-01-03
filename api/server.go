76521362b78a5d7e533e58b2c2799f61f1418a23
xoreo <mattnappo@gmail.com>
2020-01-03 09:56:52 -0500

    api: More logging.

--- api/server.go
+++ api/server.go
@@ -3,6 +3,7 @@ package api
 import (
 	"net/http"
 	"strconv"
+	"fmt"
 )
 
 // StartAPIServer starts the API server.
@@ -10,4 +11,5 @@ func StartAPIServer(port int) {
 	api := NewAPI() // Create a new API
 
 	http.ListenAndServe(":"+strconv.Itoa(port), api.Router) // Start an HTTP server
+	fmt.Printf("== API SERVER LISTENING ON PORT %d ==\n", port)
 }
